cc42b39b9a298e65066198a60daf32ae91e35752
imooc <827032783@qq.com>
2019-03-29 19:18:46 +0800

    update

---
+++ ema/src/main/java/com/ema/controller/portal/UserController.java
@@ -0,0 +1,22 @@
+package com.ema.controller.portal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 描述:
+ *
+ * @author xhsf
+ * @email 827032783@qq.com
+ * @create 2019-03-29 19:15
+ */
+@RestController
+@RequestMapping("/user/")
+public class UserController {
+
+    @RequestMapping("test.do")
+    public String test() {
+        return "xhsf for test";
+    }
+
+}
